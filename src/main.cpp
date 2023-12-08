91ed1a04af3e871c4fe995b492a78dad1c488a13
remyyyyh <remychiv.etu@gmail.com>
2023-12-08 09:37:00 +0100

    Test d'un réseau de neuronnes

--- src/main.cpp
+++ src/main.cpp
@@ -8,37 +8,114 @@
 
 #include "tools.h"
 
+
+
+
 int main()
 {
-    std::tuple<int, int, int> inputShape{6, 6, 6};
-    std::tuple<int, int, int, int, int> filtersShape{4, 2, 2, 1, 0};
 
-    auto input = xt::random::rand<float>({6,6,6}, 0, 1);  
 
-    ConvolutionLayer conv{6, inputShape, filtersShape};
+    xt::xarray<float> input = xt::random::randint({3,3,3}, 0, 10);
+    auto input2 = input;
+    // xt::xarray<float> input = xt::random::randint({2,3, 5, 5}, -10, 10);  
+    std::cout << input << '\n' << std::endl;
+    // std::cout << xt::where(input <= 0, 0.0, input) << std::endl;
+    
+    std::cout << input << '\n' << std::endl;
 
-    std::cout << input << "\n" << std::endl;
 
-    conv.forward(input);
-
-    std::cout << conv.output << "\n" << std::endl;
-
-    ReLu abc {conv.outputShape};
-
-    abc.forward(conv.output);
-    std::cout << abc.output << "\n" << std::endl;
-
-    PoolingLayer::PoolingType type = PoolingLayer::PoolingType::MAX;
-
-    PoolingLayer pool{abc.outputShape, 3, 1, 0, type};
-
-    pool.forward(abc.output);
-    std::cout << pool.output << std::endl;
-    std::cout << pool.output.shape()[0] << std::endl;
-    std::cout << pool.output.shape()[1] << std::endl;
-    std::cout << pool.output.shape()[2] << std::endl;
-
-    // std::vector <std::vector <std::vector <xt::xarray<float>>>> a ; 
 
     return 0;
+}
+
+
+
+void CNN(){
+
+    int fil1 = 32;
+    int fil2 = 64;
+    int fil3 = 128;
+    
+    xt::xarray<float> input = xt::random::randint({1,48, 58},0,2);  
+
+    // ------------------------------------------------------------------------------
+    std::tuple<int, int, int> conv1_inputShape{1, 48, 48};
+    std::tuple<int, int, int, int, int> conv1_filtersShape{fil1, 5, 5, 1, 0};
+    ConvolutionLayer conv1{1, conv1_inputShape, conv1_filtersShape};
+
+    conv1.forward(input) ;  // 32 x 44 x 44
+
+
+    std::cout << conv1.output << std::endl;
+
+
+    ReLu3D relu3d_1{conv1.outputShape};
+    relu3d_1.forward(conv1.output);
+
+    std::cout << relu3d_1.output << std::endl;
+
+    PoolingLayer pool_1{relu3d_1.outputShape,2,2,0,PoolingLayer::MAX};
+    pool_1.forward(relu3d_1.output);    // 32 x 22 x 22
+
+
+
+    // ------------------------------------------------------------------------------
+
+    std::tuple<int, int, int, int, int> conv2_filtersShape{fil2, 3, 3, 1, 0};
+    ConvolutionLayer conv2{std::get<0>(pool_1.outputShape), pool_1.outputShape, conv2_filtersShape};
+
+    ReLu3D relu3d_2{conv2.outputShape};
+    relu3d_2.forward(conv2.output);
+
+    PoolingLayer pool_2{relu3d_2.outputShape,2,2,0,PoolingLayer::MAX};
+    pool_2.forward(relu3d_2.output);
+
+    // ------------------------------------------------------------------------------
+
+    std::tuple<int, int, int, int, int> conv3_filtersShape{fil2, 3, 3, 1, 0};
+    ConvolutionLayer conv3{std::get<0>(pool_2.outputShape), pool_2.outputShape, conv3_filtersShape};
+
+    ReLu3D relu3d_3{conv3.outputShape};
+    relu3d_3.forward(conv3.output);
+
+
+    // ------------------------------------------------------------------------------
+
+    xt::xarray<float> flatted = flatten(relu3d_3.output);
+
+    // ------------------------------------------------------------------------------
+
+    DenseLayer dense1{4096, 2048};
+
+    dense1.forward(flatted);
+
+    ReLu1D relu1D1{dense1.inputShape, dense1.outputShape};
+
+    relu1D1.forward(dense1.output);
+
+    dense1.dropout(50);
+
+    // ------------------------------------------------------------------------------
+
+    DenseLayer dense2{2048, 1048};
+
+    dense2.forward(relu1D1.output);
+
+    ReLu1D relu1D2{dense2.inputShape, dense2.outputShape};
+
+    relu1D2.forward(dense2.output);
+
+    dense2.dropout(50);
+
+    // ------------------------------------------------------------------------------
+
+    DenseLayer dense3{1048, 2};
+
+    dense3.forward(relu1D2.output);
+
+    // ------------------------------------------------------------------------------
+
+    std::cout << "Prédiction" << '\n' << "---------------" << std::endl;
+
+    std::cout << dense3.output << std::endl;
 }
